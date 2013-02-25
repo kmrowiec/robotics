23bf8ce11e75e2f9081f73abac6049e5e3b68915
Kamil Mrowiec <kaamilm@gmail.com>
2013-02-25 01:57:52 +0000

    simple grid making done

--- prac1/robot.cpp
+++ prac1/robot.cpp
@@ -8,7 +8,14 @@ using namespace std;
 //0.001 for robot , 0.1 for simulator
 const float ROTATE_ERROR = 0.1;
 const float ROTATE_SLOW_SPEED = 2; //10 for robot, 5 for sim
+const int GRID_SIZE = 30;
 
+enum Heading{
+	NORTH,
+	EAST,
+	SOUTH,
+	WEST
+};
 
 class Robot{
 	
@@ -16,6 +23,10 @@ class Robot{
 	PlayerClient* client;
 	RangerProxy* sp;
 	Position2dProxy* pp;
+	int grid[GRID_SIZE][GRID_SIZE];
+	int p[5][5]; //proximity 
+	int gX, gY; //position on the grid
+	Heading h;
 	
 	/*
 	void rotate(int degrees);
@@ -30,9 +41,21 @@ class Robot{
 		sp = new RangerProxy(client, 0);
 		pp = new Position2dProxy(client,0);
 		pp->SetMotorEnable(true);
+		h = SOUTH;
+		
+		int x = 0, y = 0;
+		for(x; x<GRID_SIZE; x++){
+			for(y = 0; y<GRID_SIZE; y++){
+				 grid[x][y] = -1;
+			}
+		}
 	}
+
+void rotate90(){
 	
-	void rotate(int degrees){
+}	
+	
+void rotate(int degrees){
 
     client->Read();
     float initialAngle = rtod(pp->GetYaw());
@@ -86,6 +109,231 @@ class Robot{
 	}
 	} //end of rotate function
 
+void moveToCell(Heading dest){
+	switch(dest){
+		case NORTH:
+			move(0.6);
+			//changing xy position on the grid
+			if(h==NORTH) gY--;
+			else if(h==EAST) gX++;
+			else if(h==WEST) gX--;
+			else gY++;
+		break;
+	}
+}
+
+void move(double distance){
+    client->Read();
+    double iX, iY;
+    iX = pp->GetXPos();
+    iY = pp->GetYPos();
+    
+    double cX, cY;
+    double cDistance;
+    
+    for(;;){
+      client->Read();
+      cX = pp->GetXPos();
+      cY = pp->GetYPos();
+      //std::cout << "X: " <<cX<<"Y: "<<cY << std::endl;
+      
+      cDistance = sqrt( pow(cX-iX, 2) + pow(cY-iY,2));   
+      if(cDistance - distance >= -0.05) break;
+      pp->SetSpeed(0.2, 0);
+      
+    }
+    
+}
+
+/**
+ * Checks the neighbouring cells if they are occupied.
+ * Result of the check is saved in array 'p' :
+ * 0 when empty
+ * 1 when occupied
+ * -1 when unknown
+ * First checks the closest cell, then the 2-places away ones.
+ * (if those closer are occupied, the further are unknown).
+ * Warning! Awful function!
+ */
+void checkProximity(){
+	client->Read();
+	p[0][1] = -1;
+	p[4][1] = -1;
+	p[0][3] = -1;
+	p[4][3] = -1;
+	
+	//Front cells
+	if((*sp)[3] < 0.6 && (*sp)[4] < 0.6){
+		p[2][1] = 1;
+		p[2][0] = -1;
+		p[3][0] = -1;
+		p[1][0] = -1;
+		p[1][1] = -1;
+		p[3][1] = -1;
+		p[0][0] = -1;
+		p[4][0] = -1;
+	}else{ //if front cell is empty then all other in front can be checked
+		p[2][1] = 0;
+		if((*sp)[3] < 1.2 && (*sp)[4] < 1.2){
+			p[2][0] = 1;
+		}else{
+			p[2][0] = 0;
+		}
+		//checking diagonally to the left
+		if((*sp)[1] < 1){
+			p[1][1] = 1;
+			p[0][0] = -1;
+		}else{ //if 11 is empty
+			p[1][1] = 0;
+			if((*sp)[1] < 1.8){
+				p[0][0] = 1;
+			}else p[0][0] = 0;
+		}
+		if((*sp)[2] < 1.5){
+			p[1][0] = 1;
+		}else p[1][0] = 0;
+			
+		//checking diagonally to the right
+		if((*sp)[6] < 1){
+			p[3][1] = 1;
+			p[4][0] = -1;
+		}else{
+			p[3][1] = 0;
+			if((*sp)[6] < 1.8){
+				p[4][0] = 1;
+			}else p[4][0] = 0;
+		}
+		if((*sp)[5] < 1.5){
+			p[3][0] = 1;
+		}else p[3][0] = 0;
+		
+	}//end of checking cells to the front
+	
+	//And mirroring it for back cells
+	if((*sp)[11] < 0.6 && (*sp)[12] < 0.6){
+		p[2][3] = 1;
+		p[4][4] = -1;
+		p[1][3] = -1;
+		p[3][3] = -1;
+		p[4][3] = -1;
+		p[2][4] = -1;
+		p[0][4] = -1;
+		p[3][4] = -1;
+		p[1][4] = -1;
+	}else{ //if cell behing is empty then all other can be checked
+		p[2][3] = 0;
+		if((*sp)[11] < 1.2 && (*sp)[12] < 1.2){
+			p[2][4] = 1;
+		}else{
+			p[2][4] = 0;
+		}
+		//checking diagonally to the left
+		if((*sp)[14] < 1){
+			p[1][3] = 1;
+			p[0][4] = -1;
+		}else{ //if 11 is empty
+			p[1][3] = 0;
+			if((*sp)[14] < 1.8){
+				p[0][4] = 1;
+			}else p[0][4] = 0;
+		}
+		if((*sp)[13] < 1.5){
+			p[1][4] = 1;
+		}else p[1][4] = 0;
+			
+		//checking diagonally to the right
+		if((*sp)[9] < 1){
+			p[3][3] = 1;
+			p[4][4] = -1;
+		}else{
+			p[3][3] = 0;
+			if((*sp)[9] < 1.8){
+				p[4][4] = 1;
+			}else p[4][4] = 0;
+		}
+		if((*sp)[10] < 1.5){
+			p[3][4] = 1;
+		}else p[3][4] = 0;
+	}
+	//And finally checking the sides
+	if((*sp)[0] < 0.6 && (*sp)[15] < 0.6){
+		p[1][2] = 1;
+		p[0][2] = -1;
+	}else{
+		p[1][2] = 0;
+		if((*sp)[0] < 1.2 && (*sp)[15] < 1.2){
+			p[0][2] = 1;
+		}else p[0][2] = 0;
+	}
+	
+	if((*sp)[6] < 0.6 && (*sp)[7] < 0.6){
+		p[3][2] = 1;
+		p[4][2] = -1;
+	}else{
+		p[3][2] = 0;
+		if((*sp)[6] < 1.2 && (*sp)[7] < 1.2){
+			p[4][2] = 1;
+		}else p[4][2] = 0;
+	}
+	
+	//Printing out sensors reading and proximity.
+	//cout << *sp <<endl;
+	//cout << p[0][0] << p[1][0] << p[2][0] << p[3][0] << p[4][0] <<endl;
+	//cout << p[0][1] << p[1][1] << p[2][1] << p[3][1] << p[4][1] <<endl;
+	//cout << p[0][2] << p[1][2] << "R" << p[3][2] << p[4][2] <<endl;
+	//cout << p[0][3] << p[1][3] << p[2][3] << p[3][3] << p[4][3] <<endl;
+	//cout << p[0][4] << p[1][4] << p[2][4] << p[3][4] << p[4][4] <<endl;		
+}
+
+/**
+ * Applies the information about cells nearby to the occupancy grid. 
+ */
+void applyProximityToGrid(){
+	int sX, sY;
+	int x, y;
+	switch(h){
+		case NORTH:
+			sX = gX-2;
+			sY = gY-2;
+			for(y = 0; y <5; y++){
+				for(x = 0; x < 5; x++){
+					if(p[x][y]!=-1)
+					grid[sX+x][sY+y] =p[x][y]; 
+				}
+			}
+			break;
+		case EAST:
+			sX = gX + 2;
+			sY = gY - 2;
+			for(y = 0; y <5; y++){
+				for(x = 0; x < 5; x++){
+					grid[sX-y][sY+x] = p[x][y]; 
+				}
+			}
+			break;
+		case WEST:
+			sX = gX - 2;
+			sY = gY + 2;
+			for(y = 0; y <5; y++){
+				for(x = 0; x < 5; x++){
+					grid[sX+y][sY-x] = p[x][y]; 
+				}
+			}
+			break;
+		case SOUTH:
+			sX = gX + 2;
+			sY = gY + 2;
+			for(y = 0; y <5; y++){
+				for(x = 0; x < 5; x++){
+					grid[sX-x][sY-y] = p[x][y]; 
+				}
+			}
+			break;
+			
+	 }
+}
+
+
 private :
 /**
  * Calculates the distance between two angles.
@@ -101,10 +349,30 @@ private :
 	return dist;
 }
 
+public:
+
+void drawGrid(){
+	int x = 0, y = 0;
+	for(y; y<GRID_SIZE; y++){
+		for(x = 0; x<GRID_SIZE; x++){
+			switch(grid[x][y]){
+				case -1:
+					cout << "X";
+					break;
+				case 0:
+					cout << " ";
+					break;
+				case 1:
+					cout <<  "#";
+				break;
+					
+			}
+			//cout << grid[x][y] ;
+		}
+		cout << endl;
+	}
+}
+
 };//end of robot class
 
-int main(int argc, char *argv[]){
-	Robot r("localhost");
-	sleep(3);
-	r.rotate(60);
-}
+
---
+++ prac1/simulator.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <libplayerc++/playerc++.h>
+#include </home/kamil/robotics/prac1/robot.cpp>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+	Robot r("localhost");
+	r.client->Read();
+	sleep(1);
+	
+	//starts at the left bottom of the map
+	r.gX = 2;
+	r.gY = 27;
+	
+	//r.rotate(-10);
+	//r.move(0.3);
+	//cout << r.sp[3] << r.sp[4] <<endl;
+	//cout << r.sp <<endl;
+	
+	r.h = EAST;
+	r.checkProximity();
+	r.applyProximityToGrid();
+	int i;
+	for(i =0; i<15; i++){
+		r.moveToCell(NORTH);
+		r.checkProximity();
+		r.applyProximityToGrid();		
+	}
+	r.drawGrid();
+}
